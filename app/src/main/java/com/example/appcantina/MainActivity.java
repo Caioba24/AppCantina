9dd2425699d7eb10c271c9b5136856c2a35413ad
Caioba24 <dipietroapg@gmail.com>
2022-09-06 16:06:55 -0300

    commit deixando telas responsivas e melhorando esteticamente. Ainda sem java

--- app/src/main/java/com/example/appcantina/MainActivity.java
+++ app/src/main/java/com/example/appcantina/MainActivity.java
@@ -3,12 +3,49 @@ package com.example.appcantina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView qtdproduto;
+    ImageView plus, minus;
+    String quantidade;
+    int qtdprod;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        qtdproduto = findViewById(R.id.qtdProduto);
+        plus = findViewById(R.id.plus);
+        minus = findViewById(R.id.minus);
+
+
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                quantidade = String.valueOf(qtdproduto.getText());
+                qtdprod = Integer.parseInt(quantidade);
+                qtdprod++;
+                qtdproduto.setText(String.valueOf(qtdprod));
+            }
+        });
+
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                quantidade = String.valueOf(qtdproduto);
+                qtdprod = Integer.parseInt(quantidade);
+                if (qtdprod>0){
+                    qtdprod--;
+                }
+                qtdproduto.setText(String.valueOf(qtdprod));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/appcantina/ModelosActivity.java
@@ -0,0 +1,14 @@
+package com.example.appcantina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModelosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.modelointeiro_layout);
+    }
+}
